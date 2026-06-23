266704c97458179ce535a09cd8608655f98a76aa
Luc Casassus Latrille <l.casassus-latrille@etud.univ-pau.fr>
2026-06-23 10:54:35 +0200

    added shell.c file/ Started developpement

---
+++ shell.c
@@ -0,0 +1,13 @@
+
+
+//main function : launches the shell
+int main (int argc, char **argv){
+    //for oading config files 
+    //loop function 
+
+    shell_loop();
+
+    //cleanup for shutdown
+
+    return EXIT_SUCCESS;
+}
